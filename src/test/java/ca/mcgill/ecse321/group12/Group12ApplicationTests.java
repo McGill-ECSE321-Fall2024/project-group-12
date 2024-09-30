9e1bb034852c7aeeb0652fa028619e6338434b3f
James <jamesbmadden@gmail.com>
2024-09-30 19:46:16 -0400

    Add db dependencies

---
+++ src/test/java/ca/mcgill/ecse321/group12/Group12ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.group12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
