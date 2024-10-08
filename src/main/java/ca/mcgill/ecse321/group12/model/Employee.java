2ebc70833ae52b0267cedca6425d3ff2cec45777
James <jamesbmadden@gmail.com>
2024-10-07 21:14:10 -0400

    Put entity declaration at class and remove unused imports

--- src/main/java/ca/mcgill/ecse321/group12/model/Employee.java
+++ src/main/java/ca/mcgill/ecse321/group12/model/Employee.java
@@ -2,19 +2,11 @@
 /*This code was generated using the UMPLE 1.34.0.7242.6b8819789 modeling language!*/
 
 package ca.mcgill.ecse321.group12.model;
-import java.util.*;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 
 // line 26 "../../../../../../ReindeerGames.ump"
+@Entity
 public class Employee extends UserRole
 {
 
@@ -27,7 +19,6 @@ public class Employee extends UserRole
   //------------------------
   // CONSTRUCTOR
   //------------------------
-  @Entity
   public Employee(int aId, String aEmail, String aPassword, String aName, String aPhoneNumber)
   {
     super(aId, aEmail, aPassword, aName, aPhoneNumber);
