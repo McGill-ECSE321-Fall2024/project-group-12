ced312a9e789b9d6745386ecf5b2d62f3ab31660
Sophia <44929270+slitechs@users.noreply.github.com>
2024-10-07 20:45:00 -0400

    needs import

--- src/main/java/ca/mcgill/ecse321/group12/model/Review.java
+++ src/main/java/ca/mcgill/ecse321/group12/model/Review.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 // line 55 "../../../../../../ReindeerGames.ump"
 @Entity
