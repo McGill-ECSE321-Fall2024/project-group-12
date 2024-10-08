8e2347ddbd56b3965587b25c60a8b6dd459647af
kennedyolsen <kennedy.olsen@mail.mcgill.ca>
2024-10-07 21:12:56 -0400

    Employee

--- src/main/java/ca/mcgill/ecse321/group12/model/Employee.java
+++ src/main/java/ca/mcgill/ecse321/group12/model/Employee.java
@@ -2,6 +2,17 @@
 /*This code was generated using the UMPLE 1.34.0.7242.6b8819789 modeling language!*/
 
 package ca.mcgill.ecse321.group12.model;
+import java.util.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 
 // line 26 "../../../../../../ReindeerGames.ump"
 public class Employee extends UserRole
@@ -11,13 +22,16 @@ public class Employee extends UserRole
   // MEMBER VARIABLES
   //------------------------
 
+    
+
   //------------------------
   // CONSTRUCTOR
   //------------------------
-
+  @Entity
   public Employee(int aId, String aEmail, String aPassword, String aName, String aPhoneNumber)
   {
     super(aId, aEmail, aPassword, aName, aPhoneNumber);
+  
   }
 
   //------------------------
