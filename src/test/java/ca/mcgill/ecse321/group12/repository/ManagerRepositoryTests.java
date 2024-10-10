51b2ec2142261fc80c8cdbdf5c6007a0d2c7b530
carminvide <carmin.vide@mail.mcgill.ca>
2024-10-10 17:23:43 -0400

    managerrepositorytests

---
+++ src/test/java/ca/mcgill/ecse321/group12/repository/ManagerRepositoryTests.java
@@ -0,0 +1,44 @@
+package ca.mcgill.ecse321.group12.repository;
+
+import ca.mcgill.ecse321.group12.model.Manager;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class ManagerRepositoryTests {
+	@Autowired
+	private ManagerRepository managerRepository;
+
+	@AfterEach
+	public void clearDatabase() {
+		managerRepository.deleteAll();
+	}
+
+	@Test
+	public void testPersistAndLoadPerson() {
+		// Create person
+		String name = "Kim Jung Deux";
+		String phoneNumber = "0293729037";
+		String email = "kimmyjung2@hotmail.nk";
+		String password = "iaminlovewithtrump";
+		Manager kimmyJung2 = new Manager(0, email, password, name, phoneNumber);
+
+		// Save manager
+		kimmyJung2 = managerRepository.save(kimmyJung2);
+		int id = kimmyJung2.getId();
+
+		// Read person from database
+		Manager muffinManFromDb = managerRepository.findManagerById(id);
+
+		// Assert correct response
+		assertNotNull(kimmyJung2);
+		assertEquals(muffinManFromDb.getName(), name);
+		assertEquals(muffinManFromDb.getEmail(), email);
+		assertEquals(muffinManFromDb.getPassword(), password);
+	}
+}
