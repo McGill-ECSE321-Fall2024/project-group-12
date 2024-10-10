9123e261c418dec7c3a3ff804db723dc15add5ab
Sophia <44929270+slitechs@users.noreply.github.com>
2024-10-10 17:34:53 -0400

    remove unused import

--- src/test/java/ca/mcgill/ecse321/group12/repository/CustomerRepositoryTests.java
+++ src/test/java/ca/mcgill/ecse321/group12/repository/CustomerRepositoryTests.java
@@ -7,8 +7,6 @@ import ca.mcgill.ecse321.group12.model.Cart;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.util.Date;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
