d25dd4bf3c940f5fc45a77c35192bf1ad0279107
Sophia <44929270+slitechs@users.noreply.github.com>
2024-10-10 17:34:36 -0400

    Create CustomerRepositoryTests.java

---
+++ src/test/java/ca/mcgill/ecse321/group12/repository/CustomerRepositoryTests.java
@@ -0,0 +1,54 @@
+package ca.mcgill.ecse321.group12.repository;
+
+import ca.mcgill.ecse321.group12.model.Customer;
+import ca.mcgill.ecse321.group12.model.Wishlist;
+import ca.mcgill.ecse321.group12.model.Cart;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CustomerRepositoryTests {
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	@AfterEach
+	public void clearDatabase() {
+		customerRepository.deleteAll();
+	}
+
+	@Test
+	public void testPersistAndLoadCustomer() {
+		// Create customer
+		String name = "Kennedy Olsen";
+		String email = "kennedy@gmail.com";
+		String password = "i_love_muffins";
+		String phoneNumber = "5141234567";
+		Wishlist wishlist = new Wishlist(0);
+		Cart cart = new Cart(0);
+		Customer customer = new Customer(0, email, password, name, phoneNumber, wishlist, cart);
+
+		// Save customer
+		customer = customerRepository.save(customer);
+		int id = customer.getId();
+
+		// Read customer from database
+		Customer customerFromDb = customerRepository.findCustomerById(id);
+
+		// Assert correct response
+		assertNotNull(customer);
+		assertEquals(customerFromDb.getName(), name);
+		assertEquals(customerFromDb.getEmail(), email);
+		assertEquals(customerFromDb.getPassword(), password);
+		assertEquals(customerFromDb.getPhoneNumber(), phoneNumber);
+		assertEquals(customerFromDb.getWishlist(), wishlist);
+		assertEquals(customerFromDb.getCart(), cart);
+	}
+}
