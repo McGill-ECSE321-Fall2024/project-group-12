de4aada03c05c0e268e4102a32c6e9f4ffa875cd
James <jamesbmadden@gmail.com>
2024-10-10 16:52:15 -0400

    Test for card payment repo (WIP)

---
+++ src/test/java/ca/mcgill/ecse321/group12/repository/CardPaymentRepositoryTests.java
@@ -0,0 +1,39 @@
+package ca.mcgill.ecse321.group12.repository;
+
+import ca.mcgill.ecse321.group12.model.CardPayment;
+
+@SpringBootTest
+public class CardPaymentRepositoryTests {
+	@Autowired
+	private CardPaymentRepository cardPaymentRepository;
+
+	@AfterEach
+	public void clearDatabase() {
+		cardPaymentRepository.deleteAll();
+	}
+
+	@Test
+	public void testPersistAndLoadPerson() {
+		// Create person
+		String name = "Muffin Man";
+		String emailAddress = "muffin.man@gmail.com";
+		String password = "i_love_muffins";
+		CardPayment payment = new CardPayment();
+		muffinMan.setName(name);
+		muffinMan.setEmailAddress(emailAddress);
+		muffinMan.setPassword(password);
+
+		// Save person
+		muffinMan = personRepository.save(person);
+		int id = muffinMan.getId();
+
+		// Read person from database
+		Person muffinManFromDb = personRepository.findPersonById(id);
+
+		// Assert correct response
+		assertNotNull(person);
+		assertEquals(muffinManFromDb.getName(), name);
+		assertEquals(muffinManFromDb.getEmailAddress(), emailAddress);
+		assertEquals(muffinManFromDb.getPassword(), password);
+	}
+}
