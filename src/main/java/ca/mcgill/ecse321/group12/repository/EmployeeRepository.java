314df4b593ebfef13ae804757add61ea839f8fcc
dingamy <amyding04@gmail.com>
2024-10-10 16:49:57 -0400

    :(

---
+++ src/main/java/ca/mcgill/ecse321/group12/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package ca.mcgill.ecse321.group12.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.group12.model.Employee;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Integer>{
+
+	Employee findEmployeeById(int id);
+
+}
