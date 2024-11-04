53eb193a9bfa8c97467f5166405b3d9e739a81fd
James <jamesbmadden@gmail.com>
2024-11-04 12:22:23 -0500

    create card payment service

---
+++ src/main/java/ca/mcgill/ecse321/group12/service/CardPaymentService.java
@@ -0,0 +1,33 @@
+package ca.mcgill.ecse321.group12.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ca.mcgill.ecse321.group12.model.CardPayment;
+import ca.mcgill.ecse321.group12.repository.CardPaymentRepository;
+import jakarta.transaction.Transactional;
+
+@Service
+public class CardPaymentService {
+
+  @Autowired
+  private CardPaymentRepository repo;
+
+  @Transactional
+  public CardPayment createCardPayment (String nameOnCard, String cvc, String cardNumber, String billingAddress, boolean isSaved, String expiryDate) {
+
+    // create a new Card Payment
+    // TODO: validate credit card number?
+    CardPayment cardPayment = new CardPayment();
+    cardPayment.setNameOnCard(nameOnCard);
+    cardPayment.setCvc(cvc);
+    cardPayment.setCardNumber(cardNumber);
+    cardPayment.setBillingAddress(billingAddress);
+    cardPayment.setIsSaved(isSaved);
+    cardPayment.setExpiryDate(expiryDate);
+
+    return repo.save(cardPayment);
+
+  }
+
+}
