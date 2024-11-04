15b80dcf3ec2a362a96e2602701161cd93553d1e
julien-heng <139326103+julien-heng@users.noreply.github.com>
2024-11-04 15:37:37 -0500

    fixed exception for invalid id

--- src/test/java/ca/mcgill/ecse321/group12/service/GameServiceTests.java
+++ src/test/java/ca/mcgill/ecse321/group12/service/GameServiceTests.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.group12.service;
 
+import org.hibernate.dialect.function.array.ArrayToStringFunction;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
