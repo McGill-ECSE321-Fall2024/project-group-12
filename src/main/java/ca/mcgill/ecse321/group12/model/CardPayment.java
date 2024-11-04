96c2cab8f7878d74831be7bb42c60853f62cea6d
James <jamesbmadden@gmail.com>
2024-11-04 12:22:13 -0500

    remove unused imports

--- src/main/java/ca/mcgill/ecse321/group12/model/CardPayment.java
+++ src/main/java/ca/mcgill/ecse321/group12/model/CardPayment.java
@@ -9,8 +9,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 
 // line 91 "../../../../../../ReindeerGames.ump"
 @Entity
