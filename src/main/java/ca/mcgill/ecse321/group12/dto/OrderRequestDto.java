c68d74fba57ea05282bf802fe3b2f76e07a2d5b4
James <jamesbmadden@gmail.com>
2024-11-04 12:37:16 -0500

    Add more properties to the order request dto

--- src/main/java/ca/mcgill/ecse321/group12/dto/OrderRequestDto.java
+++ src/main/java/ca/mcgill/ecse321/group12/dto/OrderRequestDto.java
@@ -4,16 +4,83 @@ public class OrderRequestDto {
 
   // TEMPORARY: should be replaced by current auth'd user's cart
   private int cartId;
+
+  // needs order properties that won't be inferred from cart
+  private String deliveryAddress;
+
+  // needs card payment info to create the associated card payment
+  private String nameOnCard;
+  private String cvc;
+  private String cardNumber;
+  private String billingAddress;
+  private boolean isSaved;
+  private String expiryDate;
   
   @SuppressWarnings("unused")
-  private OrderRequestDto() {}
+  private OrderRequestDto () {}
 
   public int getCartId () {
     return cartId;
   }
 
+  public String getDeliveryAddress () {
+    return deliveryAddress;
+  }
+
+  public String getNameOnCard () {
+    return nameOnCard;
+  }
+
+  public String getCvc () {
+    return cvc;
+  }
+
+  public String getCardNumber () {
+    return cardNumber;
+  }
+
+  public String getBillingAddress () {
+    return billingAddress;
+  }
+
+  public boolean getIsSaved () {
+    return isSaved;
+  }
+
+  public String getExpiryDate () {
+    return expiryDate;
+  }
+
   public void setCardId (int newId) {
     cartId = newId;
+  }
+
+  public void setDeliveryAddress (String newDeliveryAddress) {
+    deliveryAddress = newDeliveryAddress;
+  }
+
+  public void setNameOnCard (String newNameOnCard) {
+    nameOnCard = newNameOnCard;
+  }
+
+  public void setCvc (String newCvc) {
+    cvc = newCvc;
+  }
+
+  public void setCardNumber (String newCardNumber) {
+    cardNumber = newCardNumber;
+  }
+
+  public void setBillingAddress (String newBillingAddress) {
+    billingAddress = newBillingAddress;
+  }
+
+  public void setIsSaved (boolean newIsSaved) {
+    isSaved = newIsSaved;
+  }
+
+  public void setExpiryDate (String newExpiryDate) {
+    expiryDate = newExpiryDate;
   }
 
 }
