354918f6c23c04341c900d4a9c42abfa09e12762
Sophia <44929270+slitechs@users.noreply.github.com>
2024-11-04 11:51:07 -0500

    formatting

--- src/main/java/ca/mcgill/ecse321/group12/controller/CartController.java
+++ src/main/java/ca/mcgill/ecse321/group12/controller/CartController.java
@@ -11,18 +11,19 @@ import ca.mcgill.ecse321.group12.service.CartService;
 
 @RestController
 public class CartController {
-    @Autowired
-    private CartService cartService;
 
-    /**
-     * Return the cart with the given ID.
-     *
-     * @param cartId The primary key of the cart to find.
-     * @return The cart with the given ID.
-     */
-    @GetMapping("/cart/{cartId}")
-    public CartResponseDto findCartById(@PathVariable int cartId) {
-        Cart cart = cartService.findCartById(cartId);
-        return new CartResponseDto(cart);
-    }
+	@Autowired
+	private CartService cartService;
+
+	/**
+	 * Return the cart with the given ID.
+	 * @param cartId The primary key of the cart to find.
+	 * @return The cart with the given ID.
+	 */
+	@GetMapping("/cart/{cartId}")
+	public CartResponseDto findCartById(@PathVariable int cartId) {
+		Cart cart = cartService.findCartById(cartId);
+		return new CartResponseDto(cart);
+	}
+
 }
