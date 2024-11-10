7623d7013261bd049eaf7db273db0bcb8175fb7f
James <jamesbmadden@gmail.com>
2024-11-09 22:42:19 -0500

    load the user from the auth token

---
+++ src/main/java/ca/mcgill/ecse321/group12/SecurityFilter.java
@@ -0,0 +1,76 @@
+package ca.mcgill.ecse321.group12;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import ca.mcgill.ecse321.group12.model.Manager;
+import ca.mcgill.ecse321.group12.model.Customer;
+import ca.mcgill.ecse321.group12.model.Employee;
+import ca.mcgill.ecse321.group12.model.UserRole;
+import ca.mcgill.ecse321.group12.repository.CustomerRepository;
+import ca.mcgill.ecse321.group12.repository.EmployeeRepository;
+import ca.mcgill.ecse321.group12.repository.ManagerRepository;
+import ca.mcgill.ecse321.group12.service.AuthService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+  
+  @Autowired
+  AuthService authService;
+  @Autowired
+  CustomerRepository customerRepo;
+  @Autowired
+  EmployeeRepository employeeRepo;
+  @Autowired
+  ManagerRepository managerRepo;
+
+  /**
+   * check for a valid token and provide the auth'd user to Controller methods
+   * @author James Madden
+   */
+  @SuppressWarnings("null")
+  @Override
+  protected void doFilterInternal (HttpServletRequest req, HttpServletResponse resp, FilterChain filterChain) throws ServletException, IOException {
+    
+    // get the auth header from the request
+    String authHeader = req.getHeader("Authorization");
+
+    // check whether there is an auth header to look at
+    if (authHeader != null) {
+      String token = authHeader.replace("Bearer ", "");
+      // get the email of the user
+      String email = authService.validateToken(token);
+      // find that user
+      UserRole user;
+      // check if they're a manager
+      Manager manager = managerRepo.findManagerByEmail(email);
+      Employee employee = employeeRepo.findEmployeeByEmail(email);
+      Customer customer = customerRepo.findCustomerByEmail(email);
+      if (manager != null) {
+        user = manager;
+      } else if (employee != null) {
+        user = employee;
+      } else {
+        user = customer;
+      }
+
+      // set the authentication for controllers to look at
+      UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+
+    }
+
+    filterChain.doFilter(req, resp);
+
+  }
+
+}
