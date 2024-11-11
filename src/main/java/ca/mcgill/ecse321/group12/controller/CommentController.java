8c4eec8357c0d42ecbc52f3dcfa864301bbe2fe8
carminvide <carmin.vide@mail.mcgill.ca>
2024-11-11 18:39:34 -0500

    added update comment methods

--- src/main/java/ca/mcgill/ecse321/group12/controller/CommentController.java
+++ src/main/java/ca/mcgill/ecse321/group12/controller/CommentController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,6 +41,7 @@ public class CommentController {
 		return allComments;
 	}
 
+
 	/**
 	 * Delete an comment.
 	 * @param comment The comment to delete.
@@ -52,12 +54,24 @@ public class CommentController {
 	/**
 	 * Create a new comment.
 	 * @param comment The comment to create.
+	 * @param review The review of the new comment.
 	 * @return The created comment, including their ID.
 	 */
 	@PostMapping("/comments")
 	public CommentResponseDto createComment(@RequestBody CommentRequestDto comment) {
-		Comment createdComment = commentService.createComment(comment.getText());
+		Comment createdComment = commentService.createComment(comment.getText(), comment.getReview());
 		return new CommentResponseDto(createdComment);
+	}
+
+	/**
+	 * Update an comment.
+	 * @param comment The comment to update.
+	 * @return The updated comment, including their ID.
+	 */
+	@PutMapping("/comments/{eid}")
+	public CommentResponseDto updateComment(@PathVariable int eid, @RequestBody CommentRequestDto comment) {
+		Comment updatedComment = commentService.updateCommentById(eid, comment.getText());
+		return new CommentResponseDto(updatedComment);
 	}
 
 }
--- src/main/java/ca/mcgill/ecse321/group12/service/CommentService.java
+++ src/main/java/ca/mcgill/ecse321/group12/service/CommentService.java
@@ -1,9 +1,12 @@
 package ca.mcgill.ecse321.group12.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ca.mcgill.ecse321.group12.model.Comment;
+import ca.mcgill.ecse321.group12.model.Review;
 import ca.mcgill.ecse321.group12.repository.CommentRepository;
 import jakarta.transaction.Transactional;
 
@@ -18,7 +21,7 @@ public class CommentService {
 	 * @param id The primary key of the comment to find.
 	 * @return The comment with the given ID.
 	 */
-	public Comment findCommentById(int id) {
+	public Comment  findCommentById(int id) {
 		Comment emp = commentRepo.findCommentById(id);
 		if (emp == null) {
 			throw new IllegalArgumentException("There is no comment with ID " + id + ".");
@@ -29,12 +32,14 @@ public class CommentService {
 	/**
 	 * Create a new comment.
 	 * @param text The text of the new comment.
+	 * @param review The review of the new comment.
 	 * @return The newly created comment.
 	 */
 	@Transactional
-	public Comment createComment(String text) {
+	public Comment createComment(String text, Review review) {
 		Comment commentToCreate = new Comment();
 		commentToCreate.setText(text);
+		commentToCreate.setReview(review);
 		return commentRepo.save(commentToCreate);
 	}
 
@@ -57,4 +62,29 @@ public class CommentService {
 		return commentRepo.findAll();
 	}
 
+	/**
+	 * Find comments by review
+	 * @param review The review to search for
+	 * @return A list of comments with the given review
+	 */
+	@Transactional
+	public Iterable<Comment> findCommentsByReview(Review review) {
+		Iterable<Comment> comments = new ArrayList<Comment>();
+		for (Comment comment : commentRepo.findAll()) {
+			if (comment.getReview().equals(review)) {
+				((ArrayList<Comment>) comments).add(comment);
+			}
+		}
+		return comments;
+	}
+
+	/**
+	 * Update the text of a comment.
+	 */
+	@Transactional
+	public Comment updateCommentById(int id, String text) {
+		Comment commentToUpdate = commentRepo.findCommentById(id);
+		commentToUpdate.setText(text);
+		return commentRepo.save(commentToUpdate);
+	}
 }
