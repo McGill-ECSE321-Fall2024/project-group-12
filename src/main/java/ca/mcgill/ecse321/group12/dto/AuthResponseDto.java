bf7a7dc1d422870f3be57b0b5c03a1dfd4be5917
James <jamesbmadden@gmail.com>
2024-11-12 13:14:23 -0500

    Create controller

---
+++ src/main/java/ca/mcgill/ecse321/group12/dto/AuthResponseDto.java
@@ -0,0 +1,15 @@
+package ca.mcgill.ecse321.group12.dto;
+
+public class AuthResponseDto {
+  
+  private String token;
+
+  public String getToken() {
+    return token;
+  }
+
+  public void setToken(String newToken) {
+    token = newToken;
+  }
+
+}
