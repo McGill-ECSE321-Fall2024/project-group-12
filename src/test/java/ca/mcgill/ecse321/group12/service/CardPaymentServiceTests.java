2115cd25d50cadab119e74a47f70a3f6d3b9b97d
julien-heng <139326103+julien-heng@users.noreply.github.com>
2024-11-13 20:22:13 -0500

    tests for cardpayment

---
+++ src/test/java/ca/mcgill/ecse321/group12/service/CardPaymentServiceTests.java
@@ -0,0 +1,57 @@
+package ca.mcgill.ecse321.group12.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ca.mcgill.ecse321.group12.model.CardPayment;
+import ca.mcgill.ecse321.group12.repository.CardPaymentRepository;
+
+@SpringBootTest
+public class CardPaymentServiceTests {
+
+	@Mock
+	private CardPaymentRepository repo;
+
+	@InjectMocks
+	private CardPaymentService service;
+
+	@Test
+	public void testCreateValidCardPayment() {
+		// Arrange
+
+		int id = 1;
+		String nameOnCard = "John Doe";
+		String cvc = "123";
+		String cardNumber = "1234 5678 9012 3456";
+		String billingAddress = "1234 Rue Sherbrooke";
+		boolean isSaved = true;
+		String expiryDate = "12/23";
+
+		CardPayment cardPayment = new CardPayment(id, nameOnCard, cvc, cardNumber, billingAddress, isSaved, expiryDate);
+
+		when(repo.save(any(CardPayment.class))).thenReturn(cardPayment);
+
+		CardPayment created = service.createCardPayment(nameOnCard, cvc, cardNumber, billingAddress, isSaved,
+				expiryDate);
+
+		// Assert
+		assertNotNull(created);
+		assertEquals(nameOnCard, created.getNameOnCard());
+		assertEquals(cvc, created.getCvc());
+		assertEquals(cardNumber, created.getCardNumber());
+		assertEquals(billingAddress, created.getBillingAddress());
+		assertEquals(isSaved, created.getIsSaved());
+		assertEquals(expiryDate, created.getExpiryDate());
+		verify(repo, times(1)).save(any(CardPayment.class));
+
+	}
+
+}
