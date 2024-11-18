7452781b688c01eddf0203062a6ac8ecf1a7d223
James <jamesbmadden@gmail.com>
2024-11-18 17:50:47 -0500

    add relevant pages for the app and add mobile navbar design

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,20 +4,39 @@ import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <NavBar />
+  <div class="app-root">
+    <NavBar />
 
-  <main class="page-container">
-    <RouterView />
-  </main>
+    <main class="page-container">
+      <RouterView />
+    </main>
+  </div>
 </template>
 
 <style scoped>
 .page-container {
+  position: relative;
   height: calc(100vh - var(--navbar-height));
   background: var(--color-background-soft);
   overflow: auto;
   border-top-left-radius: 24px;
   border-top-right-radius: 24px;
   padding: 1rem;
+}
+
+/**
+ * adapt the navbar to a mobile device
+ */
+@media only screen and (max-width: 600px) {
+  .app-root {
+    display: flex;
+    flex-direction: column-reverse;
+  }
+  .page-container {
+    border-top-left-radius: 0px;
+    border-top-right-radius: 0px;
+    border-bottom-left-radius: 24px;
+    border-bottom-right-radius: 24px;
+  }
 }
 </style>
