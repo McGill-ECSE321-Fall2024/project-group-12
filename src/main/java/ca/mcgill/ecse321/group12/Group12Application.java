68337695911570691aab4b225ef876469720e956
James <jamesbmadden@gmail.com>
2024-11-18 19:27:42 -0500

    allow CORS

--- src/main/java/ca/mcgill/ecse321/group12/Group12Application.java
+++ src/main/java/ca/mcgill/ecse321/group12/Group12Application.java
@@ -2,7 +2,10 @@ package ca.mcgill.ecse321.group12;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @PropertySource("file:${user.dir}/.env")
@@ -10,6 +13,17 @@ public class Group12Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Group12Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://localhost:5173");
+				registry.addMapping("/*/*").allowedOrigins("http://localhost:5173");
+			}
+		};
 	}
 
 }
