315468ef104a234aaa8f47361eb43251bc94c773
James <jamesbmadden@gmail.com>
2024-11-20 10:55:32 -0500

    Create store and add a simple counter as a demo

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,7 +1,12 @@
+<script setup>
+import StoreCounter from '@/components/StoreCounter.vue'
+</script>
+
 <template>
   <div class="about">
     <img src="@/assets/logo.svg" />
     <h1>About</h1>
+    <StoreCounter />
   </div>
 </template>
 
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup></script>
+<script setup>
+import StoreCounter from '@/components/StoreCounter.vue'
+</script>
 
 <template>
   <main>
     <h1>Home</h1>
     <img src="@/assets/logo.svg" width="64" />
+    <StoreCounter />
   </main>
 </template>
 
