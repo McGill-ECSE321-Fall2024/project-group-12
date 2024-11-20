ab947c15fa8c1af5cc9dfedf6e0b9089e9c43c60
James <jamesbmadden@gmail.com>
2024-11-20 18:22:18 -0500

    Add list of games to the homepage

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,6 +5,7 @@
 <script setup>
 import { inject } from 'vue'
 import BackgroundGradient from '@/components/BackgroundGradient.vue'
+import GameCard from '@/components/GameCard.vue'
 
 const { createThemeFromColour } = inject('theme')
 const { user } = inject('auth')
@@ -15,8 +16,20 @@ createThemeFromColour('#FF9797')
 <template>
   <main>
     <BackgroundGradient colour="255, 151, 151" />
-    <h1 class="header sale--text">Happy Holidays, <span class="sale--name">{{ user?.name }}!</span></h1>
+    <h1 class="header sale--text">Happy Holidays, <span class="sale--name">{{ user?.name }}</span>!</h1>
     <h2 class="subheader sale--text">Enjoy up to 50% off some of the best games of the year!</h2>
+
+    <div class="game-list">
+      <GameCard id="0" name="Legend of Zelda: Tears of the Kingd..." img="https://upload.wikimedia.org/wikipedia/en/f/fb/The_Legend_of_Zelda_Tears_of_the_Kingdom_cover.jpg" />
+      <GameCard id="0" name="Mario Kart 8" img="https://upload.wikimedia.org/wikipedia/en/b/b5/MarioKart8Boxart.jpg" />
+      <GameCard id="0" name="Animal Crossing: New Horizons" img="https://upload.wikimedia.org/wikipedia/en/1/1f/Animal_Crossing_New_Horizons.jpg" />
+      <GameCard id="0" name="Legend of Zelda: Tears of the Kingd..." img="https://upload.wikimedia.org/wikipedia/en/f/fb/The_Legend_of_Zelda_Tears_of_the_Kingdom_cover.jpg" />
+      <GameCard id="0" name="Mario Kart 8" img="https://upload.wikimedia.org/wikipedia/en/b/b5/MarioKart8Boxart.jpg" />
+      <GameCard id="0" name="Animal Crossing: New Horizons" img="https://upload.wikimedia.org/wikipedia/en/1/1f/Animal_Crossing_New_Horizons.jpg" />
+      <GameCard id="0" name="Legend of Zelda: Tears of the Kingd..." img="https://upload.wikimedia.org/wikipedia/en/f/fb/The_Legend_of_Zelda_Tears_of_the_Kingdom_cover.jpg" />
+      <GameCard id="0" name="Mario Kart 8" img="https://upload.wikimedia.org/wikipedia/en/b/b5/MarioKart8Boxart.jpg" />
+      <GameCard id="0" name="Animal Crossing: New Horizons" img="https://upload.wikimedia.org/wikipedia/en/1/1f/Animal_Crossing_New_Horizons.jpg" />
+    </div>
   </main>
 </template>
 
@@ -26,9 +39,19 @@ createThemeFromColour('#FF9797')
 }
 .sale--text {
   color: #FF9797;
+  padding-bottom: 16px;
 }
 .sale--name {
   color: white;
   font-weight: inherit;
+}
+.game-list {
+  padding-top: 8px;
+  overflow-x: auto;
+  overflow-y: visible;
+  text-wrap: nowrap;
+  position: relative;
+  left: -16px;
+  width: calc(100% + 32px);
 }
 </style>
