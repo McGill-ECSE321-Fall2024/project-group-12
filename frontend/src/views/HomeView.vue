f4442bbd0ad4bf2c044976b488e32eb419fdff66
James <jamesbmadden@gmail.com>
2024-11-20 16:51:26 -0500

    Browse games header

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,23 +4,31 @@
 -->
 <script setup>
 import { inject } from 'vue'
-import StoreCounter from '@/components/StoreCounter.vue'
+import BackgroundGradient from '@/components/BackgroundGradient.vue'
 
 const { createThemeFromColour } = inject('theme')
+const { user } = inject('auth')
 // change to a red theme to match the holiday effect
 createThemeFromColour('#FF9797')
 </script>
 
 <template>
   <main>
-    <h1>Home</h1>
-    <img class="logo" src="@/assets/logo.svg" width="64" />
-    <StoreCounter />
+    <BackgroundGradient colour="255, 151, 151" />
+    <h1 class="header sale--text">Happy Holidays, <span class="sale--name">{{ user?.name }}!</span></h1>
+    <h2 class="subheader sale--text">Enjoy up to 50% off some of the best games of the year!</h2>
   </main>
 </template>
 
 <style scoped>
 .logo {
   view-transition-name: logo;
+}
+.sale--text {
+  color: #FF9797;
+}
+.sale--name {
+  color: white;
+  font-weight: inherit;
 }
 </style>
