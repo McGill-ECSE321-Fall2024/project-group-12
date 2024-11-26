f68002612dce3516eff8c5ad79721c984fda504b
kennedyolsen <kennedy.olsen@mail.mcgill.ca>
2024-11-25 19:16:42 -0500

    Implemented the UI of Cart Page

---
+++ frontend/src/components/CartItem.vue
@@ -0,0 +1,81 @@
+<!--
+ For cart page
+ @author Kennedy Olsen
+-->
+
+<script setup></script>
+
+<template>
+    <div class="cart-item">
+      <hr /> <!--creates horizontal line-->
+      <div class="item">
+        <img class="game-cover" src="@/assets/games/minecraft.png" />
+        <div class="game-info">
+          <h2>Minecraft</h2> <!--game title-->
+          <div class="game-details">
+            <p class="game-console">PC</p> <!--PC/XBOX/nintendo etc-->
+            
+            <p class="game-year">2011</p> <!--year of release-->
+            <p class="game-price">$29.99</p> <!--price-->
+          </div>
+          <button class="remove">- Remove from Cart</button>
+        </div>
+      </div>
+      <hr />
+    </div>
+  </template>
+
+<style scoped>
+h2 {
+  color: #ffffff;
+  font-size: 32px;
+}
+button {
+  background-color: #888888;
+  color: #ffffff;
+  border: none;
+  border-radius: 100px;
+  padding: 12px;
+  margin: 16px 0px;
+}
+.remove {
+  background-color: #ffd4eb;
+  color: #ac3976;
+}
+.item {
+  margin: 12px 20px;
+}
+.game-cover {
+  max-width: 112px;
+  max-height: 155px;
+  display: inline-block;
+  margin-right: 20px;
+}
+.game-info {
+  display: inline-block;
+  vertical-align: top;
+  height: 155px;
+
+}
+.game-details {
+  font-size: 20px;
+}
+.game-console {
+  display: inline-block;
+  margin-right: 10px;
+}
+.game-year {
+  display: inline-block;
+  margin: 10px;
+}
+.game-price 
+  display: inline-block;
+  margin: 10px;
+}
+.icon {
+  width: 16px;
+  height: 16px;
+  margin: 0px 10px;
+}
+
+</style>
--- frontend/src/views/CartView.vue
+++ frontend/src/views/CartView.vue
@@ -1,23 +1,62 @@
+<!--
+ Cart page
+ @author Kennedy Olsen
+-->
+
 <script setup>
 import { inject } from 'vue'
-import StoreCounter from '@/components/StoreCounter.vue'
-
+import CartItem from '@/components/CartItem.vue'
 const { createThemeFromColour } = inject('theme')
 // change to a red theme to match the holiday effect
 createThemeFromColour('#00ff00')
 </script>
 
-<template>
-  <div class="cart">
-    <img src="@/assets/logo.svg" />
-    <h1>Cart</h1>
-    <StoreCounter />
-  </div>
+<template> 
+  <main>
+    <div class="title">
+      <img class="icon" src="@/assets/icons/navbar/cart.png" />
+      <h1>My Cart</h1>
+    </div>
+    <div class="cart-item">
+      <CartItem />
+    </div>
+    <button class="checkout">- Checkout</button>
+  </main>
+  
 </template>
 
+
 <style scoped>
-img {
-  height: 100%;
-  view-transition-name: logo;
+h1 {
+  color: #ffffff;
+  text-align: center;
+  font-size: 36px;
 }
+button {
+  background-color: #888888;
+  color: #ffffff;
+  border: none;
+  border-radius: 100px;
+  padding: 12px;
+  margin: 16px 0px;
+}
+.title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: 14px;
+}
+.icon {
+  width: 48px;
+  height: 48px;
+}
+.cart-item {
+  margin: 20px;
+}
+.checkout {
+  background-color: #da8cb5;
+  color: #ac3976;
+}
+
+
 </style>
