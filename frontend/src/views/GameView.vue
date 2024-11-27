be5d5120cfc4886f8ac268f4b4ff6846423b97b4
James <jamesbmadden@gmail.com>
2024-11-26 20:25:18 -0500

    Add buttons and dynamic theming to game page

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -1,5 +1,8 @@
 <script setup>
-import { ref } from 'vue'
+import FancyButton from '@/components/FancyButton.vue'
+import PlusIcon from 'vue-material-design-icons/Plus.vue'
+import HeartOutlineIcon from 'vue-material-design-icons/HeartOutline.vue'
+import { ref, useTemplateRef, onMounted, inject } from 'vue'
 
 const props = defineProps({
   id: {
@@ -8,9 +11,15 @@ const props = defineProps({
   }
 })
 
+const { createThemeFromImg } = inject('theme')
+
 const game = ref(null)
 
-
+// set the colour scheme
+const backgroundImg = useTemplateRef('background-img')
+onMounted(() => {
+  createThemeFromImg(backgroundImg.value)
+})
 
 // load the game
 const loadGame = async () => {
@@ -22,15 +31,29 @@ loadGame()
 
 <template>
   <div class="game-page">
-    <img class="game-background" src="@/assets/loz-poster.jpg" />
+    <img class="game-background" src="@/assets/loz-poster.jpg" ref="background-img" />
     <header class="game-info">
-      <img class="game-poster" src="https://upload.wikimedia.org/wikipedia/en/f/fb/The_Legend_of_Zelda_Tears_of_the_Kingdom_cover.jpg" />
+      <img class="game-poster"
+        src="https://upload.wikimedia.org/wikipedia/en/f/fb/The_Legend_of_Zelda_Tears_of_the_Kingdom_cover.jpg" />
       <div class="game-titles">
         <h1 class="header">{{ game ? game.name : "..." }}</h1>
         <h2 class="subheader">{{ game?.console }} • {{ game?.year }} • ${{ game?.price }} • rating</h2>
       </div>
     </header>
+
+    <!-- the background fade of the page -->
     <div class="game-bg-fade"></div>
+
+    <main class="game-content">
+      <div class="button-row">
+        <FancyButton filled label="Add to Cart">
+          <PlusIcon />
+        </FancyButton>
+        <FancyButton label="Add to Wishlist">
+          <HeartOutlineIcon />
+        </FancyButton>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -40,11 +63,28 @@ loadGame()
   position: relative;
   z-index: 1;
   display: flex;
+  padding: 0 16px;
+  top: 160px;
+  align-items: end;
+  gap: 28px;
 }
+
+.game-content {
+  position: relative;
+  top: 160px;
+}
+
 .game-poster {
   width: 200px;
   view-transition-name: game-cover;
 }
+
+.button-row {
+  display: flex;
+  gap: 16px;
+  padding: 16px;
+}
+
 .game-background {
   position: absolute;
   top: 0;
@@ -55,6 +95,7 @@ loadGame()
   z-index: 0;
   object-position: 0% 75%;
 }
+
 .game-bg-fade {
   position: absolute;
   top: 128px;
