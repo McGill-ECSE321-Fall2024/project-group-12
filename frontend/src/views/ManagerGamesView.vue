397103dcc7e50804343a490a1f5e4d8f069cb122
James <jamesbmadden@gmail.com>
2024-12-05 00:02:00 -0500

    update games

--- frontend/src/views/ManagerGamesView.vue
+++ frontend/src/views/ManagerGamesView.vue
@@ -1,19 +1,20 @@
 <script setup>
-import { inject } from 'vue'
-
+import { inject, ref } from 'vue'
 const { createThemeFromColour } = inject('theme')
-const { user } = inject('auth')
+const { token } = inject('auth')
 import { onMounted } from 'vue'
 
 
 // change to a red theme to match the holiday effect
 createThemeFromColour('#FF9797')
 
+const editDialogueOpen = ref(null)
+
 // load all games from the db
 const response = await fetch('http://localhost:8080/games')
 const games = await response.json()
 const display = async (game) => {
-    
+
     const cover = await fetch(`http://localhost:8080/games/${game.id}/cover`)
     const coverJson = await cover.json()
     const image_src = `data:image/${coverJson.type};base64,${coverJson.image}`
@@ -27,8 +28,14 @@ const display = async (game) => {
     let title = document.createElement('p')
     title.innerText = game.name
     title.setAttribute('style', "width: 100px; height: 20px; overflow-y: hidden")
+    let button = document.createElement('button')
+    button.innerText = 'edit'
+    button.onclick = () => {
+      editDialogueOpen.value = game
+    }
     child.appendChild(image)
     child.appendChild(title)
+    child.appendChild(button)
 
     document.getElementById(game.status).appendChild(child)
 }
@@ -39,40 +46,140 @@ onMounted(() => {
     }
 })
 
+const updateGame = async event => {
+
+  event.preventDefault()
+
+  const name = event.target.querySelector('#name').value
+  const description = event.target.querySelector('#description').value
+  const console = event.target.querySelector('#console').value
+  const category = event.target.querySelector('#category').value
+  const inventory = event.target.querySelector('#inventory').value
+  const price = event.target.querySelector('#price').value
+
+  // post the game
+  const response = await fetch(`http://localhost:8080/games/${editDialogueOpen.value.id}`, {
+    method: 'PUT',
+    headers: {
+      'Authorization': `Bearer ${token.value}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      ...editDialogueOpen.value,
+      name, description, console, category, inventory, price
+    })
+  })
+
+  location.reload()
+
+}
+
 </script>
 
 
 <template>
-    <div class="game-view" style="display: flex; flex-direction: column; height: fit-content; ">
-        <div style="height:200px">
+    <div v-if="editDialogueOpen == null" class="game-view" style="display: flex; flex-direction: column; height: fit-content; ">
+        <div>
             <p>In Catalog</p>
             <div id="InCatalog" style="display: flex; flex-direction: row;">
             </div>
         </div>
-        <div style="height: 200px">
+        <div>
             <p>Pending Approval</p>
             <div id="PendingApproval" style="display: flex; flex-direction: row;">
-                
+
             </div>
         </div>
-        <div style="height: 200px">
+        <div>
             <p>Pending Archival</p>
             <div id="PendingArchival" style="display: flex; flex-direction: row;">
-                
+
             </div>
         </div>
-        <div style="height: 200px">
+        <div>
             <p>Archived</p>
             <div id="Archived" style="display: flex; flex-direction: row;">
-                
+
             </div>
         </div>
-        <div style="height: 200px">
+        <div>
             <p>Rejected</p>
             <div id="Rejected" style="display: flex; flex-direction: row;">
-                
+
             </div>
         </div>
 
     </div>
+    <!-- area to edit a game -->
+    <form v-else @submit="updateGame">
+      <h1 class="subtitle">Edit Game</h1>
+      <div class="form-element">
+        <label for="name">Name</label>
+        <input id="name" :value="editDialogueOpen.name" />
+      </div>
+      <div class="form-element">
+        <label for="description">Description</label>
+        <input id="description" :value="editDialogueOpen.description" />
+      </div>
+      <div class="form-element">
+        <label for="inventory">Inventory</label>
+        <input id="inventory" type="number" :value="editDialogueOpen.inventory" />
+      </div>
+      <div class="form-element">
+        <label for="price">Price</label>
+        <input id="price" type="number" :value="editDialogueOpen.price" />
+      </div>
+      <div class="form-element">
+        <label for="console">Console</label>
+        <select id="console" :value="editDialogueOpen.console">
+          <option value="PlayStation">PlayStation</option>
+          <option value="XBox">XBox</option>
+          <option value="Switch">Switch</option>
+          <option value="PC">PC</option>
+        </select>
+      </div>
+      <div class="form-element">
+        <label for="category">Category</label>
+        <select id="category" :value="editDialogueOpen.category">
+          <option value="Adventure">Adventure</option>
+          <option value="Action">Action</option>
+          <option value="Sports">Sports</option>
+          <option value="Puzzle">Puzzle</option>
+        </select>
+      </div>
+
+      <button>Save</button>
+    </form>
 </template>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 40%;
+  margin-top: 20px;
+}
+.form-element {
+  margin-bottom: 20px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  font-size: x-large;
+}
+input {
+  height: 48px;
+  border-radius: 8px;
+  padding-left: 10px;
+}
+button {
+  width: 30%;
+  height: 48px;
+  border: none;
+  border-radius: 24px;
+  padding-left: 10px;
+  background-color: green;
+  font-size: large;
+  color: white;
+}
+</style>
