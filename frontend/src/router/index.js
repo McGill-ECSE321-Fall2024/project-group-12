529c82426be7324eabae6ef494b953470569fbe7
julien-heng <139326103+julien-heng@users.noreply.github.com>
2024-12-04 22:16:08 -0500

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,7 +4,17 @@ import CartView from '../views/CartView.vue'
 import UserView from '../views/UserView.vue'
 import WishlistView from '../views/WishlistView.vue'
 import GameView from '../views/GameView.vue'
+<<<<<<< Updated upstream
 import CheckoutView from '@/views/CheckoutView.vue'
+=======
+import ManagerAccountView from '@/views/ManagerAccountView.vue'
+import ManagerCustomerView from '@/views/ManagerCustomerView.vue'
+import SigninView from '@/views/SigninView.vue'
+import ManagerSpecificCustomer from '@/views/ManagerSpecificCustomerView.vue'
+import ManagerEmployeeView from '@/views/ManagerEmployeeView.vue'
+import ManagerSpecificEmployee from '@/views/ManagerSpecificEmployeeView.vue'
+import ManagerGameView from '@/views/ManagerGameView.vue'
+>>>>>>> Stashed changes
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -44,6 +54,12 @@ const router = createRouter({
       path: '/checkout',
       name: 'checkout',
       component: CheckoutView,
+    },
+    {
+      path: '/manager/games',
+      name: 'manager/game',
+      component: ManagerGameView,
+      props: true,
     },
   ],
 })
